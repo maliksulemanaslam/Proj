2f4863c7169e6b9e464470bc95ce44178bcb36dc
suleman <msullemanaslam@gmail.com>
2022-06-13 14:50:42 +0500

    updated

--- public/javascript/scripting.js
+++ public/javascript/scripting.js
@@ -1,18 +1,11 @@
 //jshint esversion:6
-console.log("scripting page called");
-// import { loggedinUser } from "./index.js";
-let booked;
-// const {loggedinUser} = require('./index.js')
 
+let booked;
 
 function myFunction(x) {
 
-// console.log("x from server called: " +x);
-
-// console.log("logged in user called from scripting: "+ loggedinUser);
-//console.log("func called");
 booked = x;
-// window.alert(booked);
+
 console.log("booked: "+ booked);
 
 return x;
